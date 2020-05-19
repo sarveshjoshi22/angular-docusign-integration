cbcc479c713ed3c9783d5962a9c09045b602b98a
joshisar <sarvesh_joshi@paramount.com>
2020-05-19 20:57:13 +0530

    Final code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   // docusign credentials
-  accountId: 10516189,
-  account_id: 'dea15dbe-056c-4450-a049-644b1c39de38',
+  accountId: 123456789,
+  account_id: 'dea15dbe-xxxx-4450-a049-644b1c39de38',
   authUrl: 'https://account-d.docusign.com/oauth/auth',
   baseUrlInfo: '/oauth/userinfo',
   baseUrl: '/restapi/v2.1/accounts',
-  clientId: 'c0f73303-538a-4f3b-88d5-5a1ed56a4db3',
+  clientId: 'c0f73303-xxxx-4f3b-88d5-5a1ed56a4db3',
   username: 'sarvesh.joshi@lntinfotech.com',
-  password: 'Hn8L7gmCWg87S9TudhyAnE7Dnq8=',
+  password: 'xxxxx=',
   redirectUrl: 'http://127.0.0.1:4200/setState/'
 };
